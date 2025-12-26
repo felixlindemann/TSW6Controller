991d99b5bd1697eb1cc55b79c98f4fef7ec9c33e
Felix Lindemann <felixlindemann@web.de>
2025-12-26 12:54:20 +0100

    Reboot Arduino from Angular.

--- angular-API-Explorer/tswApiExplorer/src/app/navbar/navbar.component.ts
+++ angular-API-Explorer/tswApiExplorer/src/app/navbar/navbar.component.ts
@@ -1,11 +1,35 @@
+
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ArduinoApiService } from '../core/arduino-api.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  providers: [ArduinoApiService]
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  showModal = false;
+
+  constructor(private arduinoApi: ArduinoApiService) {}
+
+  onRebootClick() {
+    this.showModal = true;
+    this.arduinoApi.reboot().subscribe({
+      next: () => {
+        setTimeout(() => {
+          this.showModal = false;
+        }, 10000);
+      },
+      error: () => {
+        setTimeout(() => {
+          this.showModal = false;
+        }, 10000);
+      }
+    });
+  }
+}
